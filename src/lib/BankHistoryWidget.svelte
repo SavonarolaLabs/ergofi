1e330960b2a8af79fcf32716f1c1e0a410737de8
c8e4 <c8e4@proton.me>
2024-11-25 17:22:54 +0100

    history, wallet button

---
+++ src/lib/BankHistoryWidget.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { formatDistanceToNowStrict } from 'date-fns';
+
+	type Interaction = {
+		id: string;
+		amount: number;
+		timestamp: number;
+		price: number;
+		type: 'Mint' | 'Redeem';
+		ergAmount: number;
+	};
+
+	const interactions: Interaction[] = [
+		{
+			id: 'abc123def',
+			amount: 100.5,
+			timestamp: Date.now() - 7200000,
+			type: 'Mint',
+			ergAmount: -50.25,
+			price: 2.01
+		},
+		{
+			id: 'ghi456jkl',
+			amount: -200,
+			timestamp: Date.now() - 18000000,
+			type: 'Redeem',
+			ergAmount: 100,
+			price: 1.95
+		}, // 5 hours ago
+		{
+			id: 'mno789pqr',
+			amount: 150.123,
+			timestamp: Date.now() - 86400000,
+			type: 'Mint',
+			ergAmount: -75.0615,
+			price: 2.03
+		} // 1 day ago
+	];
+
+	const shortTransactionId = (id: string): string => `${id.slice(0, 3)}...${id.slice(-3)}`;
+	const formatTimeAgo = (timestamp: number): string => {
+		const time = formatDistanceToNowStrict(new Date(timestamp));
+		return (
+			time
+				.replace(/ hours?/, 'h')
+				.replace(/ minutes?/, 'm')
+				.replace(/ days?/, 'd')
+				.replace(/ seconds?/, 's') + ' ago'
+		);
+	};
+	const formatAmount = (amount: number): string => `${amount > 0 ? '+' : ''}${amount.toFixed(2)}`;
+</script>
+
+<div class="widget">
+	{#each interactions as interaction}
+		<div class="row">
+			<div class="left pb-1">
+				<div>
+					<div class="uppercase text-gray-400">{interaction.type} @{interaction.price}</div>
+				</div>
+				<span class="text-sm text-gray-500">{formatTimeAgo(interaction.timestamp)}</span>
+			</div>
+			<div class="flex flex-col">
+				<div>
+					<span class="mr-1 text-3xl font-light">
+						{formatAmount(interaction.amount)}
+					</span>
+					<span class="text-lg font-light text-gray-500"> SigUSD </span>
+				</div>
+				<div class="pr-9 text-right text-gray-500">
+					{formatAmount(interaction.ergAmount)}&nbsp&nbsp&nbspERG
+				</div>
+			</div>
+		</div>
+	{/each}
+	<h1 class="mb-2 text-9xl text-gray-700">SigmaUSD</h1>
+</div>
+
+<style>
+	.widget {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		width: 400px;
+	}
+	.row {
+		display: flex;
+		justify-content: space-between;
+		align-items: end;
+		padding: 0.5rem 1rem;
+	}
+	.left {
+		display: flex;
+		flex-direction: column;
+	}
+	.transaction-id {
+		cursor: pointer;
+		text-decoration: underline;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
 <script>
 	import Elixir from '$lib/Elixir.svelte';
 	import SwapWidget from '$lib/SwapWidget.svelte';
+	import BankHistoryWidget from '$lib/BankHistoryWidget.svelte';
+	import WalletButton from '$lib/WalletButton.svelte';
 </script>
 
-<div class="mt-20"></div>
-<SwapWidget></SwapWidget>
-<div class="mb-20"></div>
+<nav class="sticky top-0 flex items-center justify-between px-4 py-2">
+	<div class="flex space-x-4"></div>
+	<div>
+		<WalletButton></WalletButton>
+	</div>
+</nav>
 
-<div>Amount u can choose</div>
-<div>Price only for View</div>
-<div>Fee</div>
-<div>Total u receive</div>
+<div class="flex h-full pb-10">
+	<div class="flex grow items-end justify-start">
+		<BankHistoryWidget></BankHistoryWidget>
+	</div>
+	<div class="justify-left flex grow items-center">
+		<div>
+			<SwapWidget></SwapWidget>
+		</div>
+	</div>
+</div>
+
 <div class="mt-20"></div>
 <Elixir></Elixir>

f9fcfc888d804081ff508c47bd6825c54babee18
Savonarola <x@x.com>
2024-11-26 18:21:39 +0400

    table usd rsv

--- src/lib/TransactionHistory.svelte
+++ src/lib/TransactionHistory.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { history } from '../data/history';
-	import { TOKEN_SIGRSV, TOKEN_SIGUSD } from './sigmaUSD';
+	import { SIGUSD_BANK, TOKEN_SIGRSV, TOKEN_SIGUSD } from './sigmaUSD';
 
 	interface TokenStats {
 		input: number;
@@ -10,14 +10,48 @@
 
 	function calculateTokenStats(tx: any, tokenId: string): TokenStats {
 		const inputAmount = tx.inputs
+			.filter((input: any) => input.address == SIGUSD_BANK)
 			.flatMap((input: any) => input.assets)
 			.filter((asset: any) => asset.tokenId === tokenId)
 			.reduce((sum: number, asset: any) => sum + asset.amount, 0);
 
 		const outputAmount = tx.outputs
+			.filter((input: any) => input.address == SIGUSD_BANK)
 			.flatMap((output: any) => output.assets)
 			.filter((asset: any) => asset.tokenId === tokenId)
 			.reduce((sum: number, asset: any) => sum + asset.amount, 0);
+
+		return {
+			input: inputAmount,
+			output: outputAmount,
+			difference: outputAmount - inputAmount
+		};
+	}
+
+	function nanoErgToErg(nanoErg: number) {
+		return nanoErg ? Number((nanoErg / 10 ** 9).toFixed(2)) : 0;
+	}
+	function ergToNanoErg(erg: number) {
+		return erg ? erg * 10 ** 9 : 0;
+	}
+
+	function centsToUsd(cents: number) {
+		return cents ? Number((cents / 10 ** 2).toFixed(2)) : 0;
+	}
+	function usdToCents(usd: number) {
+		return usd ? usd * 10 ** 2 : 0;
+	}
+
+	function calculateErgoStats(tx: any): TokenStats {
+		const inputAmount = tx.inputs
+			.filter((input: any) => input.address == SIGUSD_BANK)
+			.flatMap((input: any) => input.value)
+			.reduce((sum: number, value: any) => sum + value, 0);
+
+		const outputAmount = tx.outputs
+			.filter((output: any) => output.address == SIGUSD_BANK)
+			.flatMap((output: any) => output.value)
+			.reduce((sum: number, value: any) => sum + value, 0);
 
 		return {
 			input: inputAmount,
@@ -38,11 +72,48 @@
 
 			const sigUSDStats = calculateTokenStats(tx, TOKEN_SIGUSD);
 			const sigRSVStats = calculateTokenStats(tx, TOKEN_SIGRSV);
+			const ergoStats = calculateErgoStats(tx);
+			let priceErgUsd;
+			let priceErgSigRSV;
+			let pair;
+			let operation;
+			let amount;
+			let volume;
+
+			if (ergoStats.difference != 0 && sigUSDStats.difference != 0) {
+				priceErgUsd = (-(sigUSDStats.difference / ergoStats.difference) * 10 ** 7).toFixed(2);
+			} else {
+				priceErgUsd = 0;
+			}
+
+			if (ergoStats.difference != 0 && sigRSVStats.difference != 0) {
+				priceErgSigRSV = (-(sigRSVStats.difference / ergoStats.difference) * 10 ** 9).toFixed(2);
+			} else {
+				priceErgSigRSV = 0;
+			}
+
+			if (sigUSDStats.difference != 0) {
+				pair = 'USD/ERG';
+				operation = sigUSDStats.difference < 0 ? 'buy' : 'sell';
+				amount = -sigUSDStats.difference + ' USD';
+			} else {
+				pair = 'RSV/ERG	';
+				operation = sigRSVStats.difference < 0 ? 'buy' : 'sell';
+				amount = -sigRSVStats.difference + ' RSV';
+			}
+			volume = -nanoErgToErg(ergoStats.difference) + ' ERG';
 
 			return {
 				...tx,
 				sigUSDStats,
 				sigRSVStats,
+				ergoStats,
+				priceErgUsd,
+				pair,
+				amount,
+				operation,
+				volume,
+				priceErgSigRSV,
 				shortenedId: shorten(tx.id),
 				shortenedAddress: shorten(tx.inputs[0]?.address),
 				formattedTimestamp: tx.timestamp ? new Date(tx.timestamp).toISOString() : ''
@@ -54,31 +125,44 @@
 <table>
 	<thead>
 		<tr>
-			<th>Tx ID</th>
-			<th>Inclusion Height</th>
 			<th>Timestamp</th>
+			<th>Tx ID</th>
+			<th>Pair</th>
+			<th>Operation</th>
+			<!-- <th>Inclusion Height</th> -->
+			<!-- <th>SIGUSD Input</th>
+			<th>SIGUSD Output</th> -->
+			<th>Amount</th>
+			<!-- <th>SIGRSV Input</th>
+			<th>SIGRSV Output</th> -->
+			<!-- <th>SIGRSV Diff</th> -->
+			<!-- <th>ERGO Input</th> -->
+			<!-- <th>ERGO Output</th> -->
+			<th>Volume</th>
+			<th>Price</th>
 			<th>Address</th>
-			<th>SIGUSD Input</th>
-			<th>SIGUSD Output</th>
-			<th>SIGUSD Diff</th>
-			<th>SIGRSV Input</th>
-			<th>SIGRSV Output</th>
-			<th>SIGRSV Diff</th>
 		</tr>
 	</thead>
 	<tbody>
 		{#each processedHistory as tx}
 			<tr>
-				<td>{tx.shortenedId}</td>
-				<td>{tx.inclusionHeight}</td>
 				<td>{tx.formattedTimestamp}</td>
+				<td>{tx.shortenedId}</td>
+				<td>{tx.pair}</td>
+				<td>{tx?.operation}</td>
+				<!-- <td>{tx.inclusionHeight}</td> -->
+				<!-- <td>{centsToUsd(tx.sigUSDStats.input)}</td>
+				<td>{centsToUsd(tx.sigUSDStats.output)}</td> -->
+				<td>{tx.amount} </td>
+				<!-- <td>{tx.sigRSVStats.input}</td>	
+				<td>{tx.sigRSVStats.output}</td> -->
+				<!-- <td>{tx.sigRSVStats.difference}</td> -->
+				<!-- <td>{nanoErgToErg(tx.ergoStats.input)}</td>
+				<td>{nanoErgToErg(tx.ergoStats.output)}</td> -->
+				<td>{tx.volume}</td>
+				<td>{tx.pair == 'USD/ERG' ? tx?.priceErgUsd : tx?.priceErgSigRSV}</td>
+
 				<td>{tx.shortenedAddress}</td>
-				<td>{tx.sigUSDStats.input}</td>
-				<td>{tx.sigUSDStats.output}</td>
-				<td>{tx.sigUSDStats.difference}</td>
-				<td>{tx.sigRSVStats.input}</td>
-				<td>{tx.sigRSVStats.output}</td>
-				<td>{tx.sigRSVStats.difference}</td>
 			</tr>
 		{/each}
 	</tbody>

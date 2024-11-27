e82762fdb02b8c553bb458e085b601f4f5634469
c8e4 <c8e4@proton.me>
2024-11-27 12:21:14 +0100

    unconfirmed tx spinner

--- src/lib/BankHistoryWidget.svelte
+++ src/lib/BankHistoryWidget.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { formatDistanceToNowStrict } from 'date-fns';
+	import Spinner from './Spinner.svelte';
 
 	type Interaction = {
 		id: string;
@@ -8,16 +9,27 @@
 		price: number;
 		type: 'Buy' | 'Sell';
 		ergAmount: number;
+		confirmed: boolean;
 	};
 
 	const interactions: Interaction[] = [
+		{
+			id: 'abc123def',
+			amount: 100000.0,
+			timestamp: Date.now(),
+			type: 'Sell',
+			ergAmount: -100,
+			price: 10.01,
+			confirmed: false
+		},
 		{
 			id: 'abc123def',
 			amount: 100.5,
 			timestamp: Date.now() - 7200000,
 			type: 'Sell',
 			ergAmount: -50.25,
-			price: 2.01
+			price: 2.01,
+			confirmed: true
 		},
 		{
 			id: 'Buy',
@@ -25,7 +37,8 @@
 			timestamp: Date.now() - 18000000,
 			type: 'Buy',
 			ergAmount: 100,
-			price: 1.95
+			price: 1.95,
+			confirmed: true
 		}, // 5 hours ago
 		{
 			id: 'mno789pqr',
@@ -33,7 +46,8 @@
 			timestamp: Date.now() - 86400000,
 			type: 'Sell',
 			ergAmount: -75.0615,
-			price: 2.03
+			price: 2.03,
+			confirmed: true
 		} // 1 day ago
 	];
 
@@ -56,7 +70,24 @@
 		<div class="row">
 			<div class="left pb-1">
 				<div>
-					<div class="uppercase text-gray-400">{interaction.type} @{interaction.price}</div>
+					<div class="flex items-center gap-1 uppercase text-gray-400">
+						{#if interaction.confirmed}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 512 512"
+								width="1em"
+								fill="currentColor"
+								style="margin-left:2px;margin-right:2px;"
+								><path
+									d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"
+								/></svg
+							>
+							{interaction.type} @{interaction.price}
+						{:else}
+							<Spinner size={16} />
+							{interaction.type} @{interaction.price}
+						{/if}
+					</div>
 				</div>
 				<span class="text-sm text-gray-500">{formatTimeAgo(interaction.timestamp)}</span>
 			</div>
@@ -67,8 +98,8 @@
 					</span>
 					<span class="text-lg font-light text-gray-500"> SigUSD </span>
 				</div>
-				<div class="pr-9 text-right text-gray-500">
-					{formatAmount(interaction.ergAmount)}&nbsp&nbsp&nbspERG
+				<div class="pr-10 text-right text-gray-500">
+					{formatAmount(interaction.ergAmount)} <span style="margin-left:7px;">ERG</span>
 				</div>
 			</div>
 		</div>
@@ -81,7 +112,7 @@
 		display: flex;
 		flex-direction: column;
 		gap: 1rem;
-		width: 400px;
+		width: 430px;
 	}
 	.row {
 		display: flex;
---
+++ src/lib/Spinner.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let size = 16;
+</script>
+
+<span
+	class="loader"
+	style="
+		width: {size}px; 
+		height: {size}px; 
+		border-width: {Math.round(size / 10)}px; 
+		border-color: currentColor;"
+></span>
+
+<style>
+	.loader {
+		border-style: dotted;
+		border-radius: 50%;
+		display: inline-block;
+		position: relative;
+		box-sizing: border-box;
+		animation: rotation 2s linear infinite;
+	}
+
+	@keyframes rotation {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>

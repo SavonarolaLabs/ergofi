441574d35d7cb98241bef96e0411a1e70e4af70b
c8e4 <c8e4@proton.me>
2025-01-08 01:03:06 +0100

    join mempool:oracle_boxes

--- src/lib/Elixir.svelte
+++ src/lib/Elixir.svelte
@@ -1,57 +1,58 @@
 <script lang="ts">
-	import { Socket } from 'phoenix';
 	import { onMount } from 'svelte';
-	import {
-		handleMempoolSocketUpdate,
-		initHistory,
-		mempool_interactions
-	} from './stores/preparedInteractions';
+	import { Socket } from 'phoenix';
+	import { handleMempoolSocketUpdate, initHistory } from './stores/preparedInteractions';
 
 	onMount(() => {
 		const socket = new Socket('wss://ergfi.xyz:4004/socket', { params: {} });
 		socket.connect();
 
-		// Choose which channel you want (options are "transactions" or "sigmausd_transactions")
-		const channelTopic = 'sigmausd_transactions';
-		const channelName = `mempool:${channelTopic}`;
-		const channel = socket.channel(channelName, {});
+		// Handle sigmausd_transactions channel
+		const sigmausdChannelTopic = 'sigmausd_transactions';
+		const sigmausdChannelName = `mempool:${sigmausdChannelTopic}`;
+		const sigmausdChannel = socket.channel(sigmausdChannelName, {});
 
-		// Join the channel
-		channel
+		sigmausdChannel
 			.join()
 			.receive('ok', (resp) => {
-				console.log(`Joined successfully ${channelName}`);
+				console.log(`Joined successfully ${sigmausdChannelName}`);
 				initHistory(resp.history);
+				console.log('sigmausd_transactions:', resp.history);
 				handleMempoolSocketUpdate(resp);
 			})
 			.receive('error', (resp) => {
-				console.log('Unable to join:', resp);
+				console.error('Unable to join sigmausd_transactions:', resp);
 			});
 
-		// channel.on('all_transactions', (payload) => {
-		// 	handleMempoolSocketUpdate(payload);
-		// });
-		channel.on(channelTopic, (payload) => {
+		sigmausdChannel.on(sigmausdChannelTopic, (payload) => {
 			handleMempoolSocketUpdate(payload);
 		});
 
-		// Cleanup when component unmounts
+		// Handle oracle_boxes channel
+		const oracleBoxesChannelTopic = 'oracle_boxes';
+		const oracleBoxesChannelName = `mempool:${oracleBoxesChannelTopic}`;
+		const oracleBoxesChannel = socket.channel(oracleBoxesChannelName, {});
+
+		oracleBoxesChannel
+			.join()
+			.receive('ok', (resp) => {
+				console.log(`Joined successfully ${oracleBoxesChannelName}`);
+				//handleMempoolSocketUpdate(resp);
+			})
+			.receive('error', (resp) => {
+				console.error('Unable to join oracle_boxes:', resp);
+			});
+
+		oracleBoxesChannel.on(oracleBoxesChannelTopic, (payload) => {
+			console.log('Update received for oracle_boxes:', payload);
+			//handleMempoolSocketUpdate(payload);
+		});
+
+		// Cleanup on component unmount
 		return () => {
-			channel.leave();
+			sigmausdChannel.leave();
+			oracleBoxesChannel.leave();
 			socket.disconnect();
 		};
 	});
 </script>
-
-<div style="display:none">
-	<h1>SigmaUSD Transactions</h1>
-	{#if $mempool_interactions.length > 0}
-		<ul>
-			{#each $mempool_interactions as tx}
-				<li><strong>Transaction ID:</strong> {tx.transactionId}</li>
-			{/each}
-		</ul>
-	{:else}
-		<p>No transactions in the mempool.</p>
-	{/if}
-</div>

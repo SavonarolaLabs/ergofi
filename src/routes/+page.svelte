05500b9c4acecbfde21c5f358aca6aa087f29624
c8e4 <c8e4@proton.me>
2025-01-13 00:53:58 +0100

    add mobile detection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,40 @@
 	import SwapWidget from '$lib/SwapWidget.svelte';
 	import BankHistoryWidget from '$lib/BankHistoryWidget.svelte';
 	import Navbar from '$lib/Navbar.svelte';
+	import { onMount } from 'svelte';
+
+	let isMobile = false;
+
+	// Detect mobile device
+	onMount(() => {
+		isMobile =
+			/Mobi|Android|iPhone|iPad|iPod/.test(navigator.userAgent) ||
+			window.matchMedia('(max-width: 768px)').matches;
+	});
 </script>
 
-<div class="tachikoma-bg flex flex-col" style="height:100vh;">
-	<Navbar></Navbar>
-	<div class="flex grow">
-		<div class="flex flex-col">
-			<div class="flex grow items-end justify-start">
-				<BankHistoryWidget></BankHistoryWidget>
+{#if isMobile}
+	<BankHistoryWidget></BankHistoryWidget>
+	<div style="visibility:hidden">
+		<SwapWidget></SwapWidget>
+	</div>
+{:else}
+	<div class="tachikoma-bg flex flex-col" style="height:100vh;">
+		<Navbar></Navbar>
+		<div class="flex grow">
+			<div class="flex flex-col">
+				<div class="flex grow items-end justify-start">
+					<BankHistoryWidget></BankHistoryWidget>
+				</div>
 			</div>
-		</div>
-		<div class="justify-left flex grow items-center">
-			<div style="margin-top:-10%; margin-left:20%;">
-				<SwapWidget></SwapWidget>
+			<div class="justify-left flex grow items-center">
+				<div style="margin-top:-10%; margin-left:20%;">
+					<SwapWidget></SwapWidget>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/if}
 
 <Elixir></Elixir>
 

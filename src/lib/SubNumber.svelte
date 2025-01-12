8a046832d506cc95bc20dcc66119b76aae117422
c8e4 <c8e4@proton.me>
2025-01-12 17:06:12 +0100

    SubNumber

---
+++ src/lib/SubNumber.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let value;
+
+	function formatSmallNumber(num: number) {
+		const str = num.toString();
+		const match = str.match(/^0\.(0+)/); // Match leading zeros after the decimal
+		const zeros = match ? match[1].length : 0;
+		const significantDigits = str.slice(zeros + 2); // Remove "0." and leading zeros
+		return { zeros, significantDigits };
+	}
+</script>
+
+{#if value != undefined}
+	{#if value < 0.01}
+		<span>
+			0.0<sub>{formatSmallNumber(value).zeros}</sub>{formatSmallNumber(value).significantDigits}
+		</span>
+	{:else}
+		{value}
+	{/if}
+{/if}

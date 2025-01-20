cafd8ed73fc6095f9da4de747dffffe4e86a6291
Savonarola <x@x.com>
2025-01-20 23:35:01 +0400

    add negative Test lpSwap

--- src/lib/dexygold/tests/lpSwap.spec.ts
+++ src/lib/dexygold/tests/lpSwap.spec.ts
@@ -118,5 +118,80 @@ describe('LpSwapSpec', () => {
 			amount: calculatedBuyY
 		});
 	});
+
+	it('Taking too many dexy should fail', () => {
+		const lpBalance = 100_000_000n;
+		const reservesXIn = 1_000_000_000_000n;
+		const reservesYIn = 100_000_000n;
+
+		const rate = Number(reservesYIn) / Number(reservesXIn);
+		const sellX = 10_000_000n;
+
+		let calculatedBuyY =
+			(BigInt(Math.floor((Number(sellX) * rate * Number(feeNumLp)) / Number(feeDenomLp))) * 101n) /
+			100n;
+
+		//expect(calculatedBuyY).toBe(996n);
+
+		const reservesXOut = reservesXIn + sellX;
+		const reservesYOut = reservesYIn - calculatedBuyY;
+
+		const deltaReservesX = reservesXOut - reservesXIn;
+		const deltaReservesY = reservesYOut - reservesYIn;
+
+		const lhs = reservesYIn * deltaReservesX * feeNumLp;
+		const rhs = -deltaReservesY * (reservesXIn * feeDenomLp + deltaReservesX * feeNumLp);
+
+		expect(lhs < rhs).toBe(true);
+
+		fundingParty.addBalance({
+			nanoergs: fakeNanoErgs
+		});
+
+		lpParty.addBalance({
+			nanoergs: reservesXIn,
+			tokens: [
+				{ tokenId: lpNFT, amount: 1n },
+				{ tokenId: lpToken, amount: lpBalance },
+				{ tokenId: dexyUSD, amount: reservesYIn }
+			]
+		});
+
+		swapParty.addBalance({
+			nanoergs: minStorageRent,
+			tokens: [{ tokenId: lpSwapNFT, amount: 1n }]
+		});
+
+		const height = mockChain.height;
+
+		const tx = new TransactionBuilder(height)
+			.from([...lpParty.utxos, ...swapParty.utxos, ...fundingParty.utxos], {
+				ensureInclusion: true
+			})
+			.to(
+				new OutputBuilder(reservesXOut, lpErgoTree).addTokens([
+					{ tokenId: lpNFT, amount: 1n },
+					{ tokenId: lpToken, amount: lpBalance },
+					{ tokenId: dexyUSD, amount: reservesYOut }
+				])
+			)
+			.to(
+				new OutputBuilder(minStorageRent, swapErgoTree).addTokens([
+					{ tokenId: lpSwapNFT, amount: 1n }
+				])
+			)
+			.to(
+				new OutputBuilder(dummyNanoErgs, userParty.address).addTokens([
+					{ tokenId: dexyUSD, amount: calculatedBuyY }
+				])
+			)
+
+			.payFee(fee)
+			.sendChangeTo(fundingParty.address)
+			.build();
+
+		const executed = mockChain.execute(tx, { throw: false });
+		expect(executed).toBe(false);
+	});
 	// Scala => JS
 });

f454ed15d5ddba4cbf8e7fd75cdf0056dedbda1f
Savonarola <x@x.com>
2025-01-24 17:39:50 +0400

    tracking positive

--- src/lib/dexygold/tests/TrackingSpec.spec.ts
+++ src/lib/dexygold/tests/TrackingSpec.spec.ts
@@ -14,7 +14,7 @@ describe('TrackingSpec', () => {
 	let mockChain: MockChain;
 
 	const { fakeScriptErgoTree, lpErgoTree, trackingErgoTree } = vitestErgoTrees;
-	const { oraclePoolNFT, lpNFT, lpToken, dexyUSD, tracking98NFT } = vitestTokenIds;
+	const { oraclePoolNFT, lpNFT, lpToken, dexyUSD, tracking98NFT, tracking101NFT } = vitestTokenIds;
 	const dummyTokenId = '0000005aa0d95f5d54a7bc89c46730d9662397067250aa18a0039631c0f5b801';
 
 	const fakeNanoErgs = 10_000_000_000_000n;
@@ -35,111 +35,20 @@ describe('TrackingSpec', () => {
 		mockChain.reset();
 	});
 
-	it.skip('Trigger 98% tracker should work', () => {
+	it.only('Trigger 98% tracker should work', () => {
+		// Tracking98 => lpRate < 98 * Oracle
+		// Tracking98  R6: true
 		const lpInCirc = 10_000n;
-		const oracleRateXY = 10_205n; //* 1_000_000n; //<== ??????
+		const oracleRateXY = 10_205n * 1_000_000n;
 		const lpBalance = 10_000_000n;
 		const reservesX = 10_000_000_000n;
 		const reservesY = 1_000_000n;
-		const numIn = 98n;
-		const denomIn = 100n;
-		const lpRateXY = reservesX / reservesY;
-		const x = lpRateXY * denomIn;
-		const y = numIn * oracleRateXY; // / 1_000_000n;  //<== ??????
-		expect(x < y).toBe(true);
-		console.log('lpRateXY', lpRateXY);
-		console.log('x', x, ' vs ', y, ' y');
-		console.log('LP rate < 98% Oracle');
-
-		const trackingHeightOut = BigInt(mockChain.height) + 1n; // <==
-		const trackingHeightIn = '2147483647'; //<== intMax / intMaxHex
-		//console.log('R7?:', SInt(Number(trackingHeightIn)).toHex());
-
-		const fundingParty = mockChain.addParty(fakeScriptErgoTree, 'Funding');
-		fundingParty.addBalance({
-			nanoergs: fakeNanoErgs
-		});
-
-		const oracleParty = mockChain.addParty(fakeScriptErgoTree, 'Oracle');
-		oracleParty.addBalance(
-			{
-				nanoergs: minStorageRent,
-				tokens: [{ tokenId: oraclePoolNFT, amount: 1n }]
-			},
-			{
-				R4: SLong(oracleRateXY).toHex()
-			}
-		);
-
-		const lpParty = mockChain.addParty(lpErgoTree, 'Lp');
-		lpParty.addBalance(
-			{
-				nanoergs: reservesX,
-				tokens: [
-					{ tokenId: lpNFT, amount: 1n },
-					{ tokenId: lpToken, amount: lpBalance },
-					{ tokenId: dexyUSD, amount: reservesY }
-				]
-			}
-			// {
-			// 	R4: SLong(lpInCirc).toHex()
-			// }
-		);
-
-		const trackingParty = mockChain.addParty(trackingErgoTree, 'Tracking');
-		trackingParty.addBalance(
-			{
-				nanoergs: minStorageRent,
-				tokens: [{ tokenId: tracking98NFT, amount: 1n }]
-			},
-			{
-				R4: SInt(Number(numIn)).toHex(),
-				R5: SInt(Number(denomIn)).toHex(),
-				R6: SBool(true).toHex(),
-				R7: intMaxHex
-				//R7: SInt(Number(trackingHeightIn)).toHex()
-			}
-		);
-
-		const trackingOut = new OutputBuilder(minStorageRent, trackingErgoTree)
-			.addTokens([{ tokenId: tracking98NFT, amount: 1n }])
-			.setAdditionalRegisters({
-				R4: SInt(Number(numIn)).toHex(),
-				R5: SInt(Number(denomIn)).toHex(),
-				R6: SBool(true).toHex(),
-				R7: SInt(Number(trackingHeightOut)).toHex()
-			});
-
-		const mainInputs = [...trackingParty.utxos, ...fundingParty.utxos];
-		const dataInputs = [...oracleParty.utxos, ...lpParty.utxos];
-
-		const tx = new TransactionBuilder(mockChain.height)
-			.from(mainInputs, {
-				ensureInclusion: true
-			})
-			.withDataFrom(dataInputs)
-			.to(trackingOut)
-			.payFee(fee)
-			.sendChangeTo(changeAddress)
-			.build();
-
-		//console.dir(tx.toEIP12Object(), { depth: null });
-		const executed = mockChain.execute(tx, { throw: false });
-		expect(executed).toBe(true);
-	});
-
-	it.only('Trigger 101% tracker should work', () => {
-		const lpInCirc = 10_000n;
-		const oracleRateXY = 9_088n * 1_000_000n;
-		const lpBalance = 10_000_000n;
-		const reservesX = 10_000_000_000n;
-		const reservesY = 1_000_000n;
-		const numIn = 101n;
+		const numIn = 98n; // 101
 		const denomIn = 100n;
 		const lpRateXY = reservesX / reservesY;
 		const x = lpRateXY * denomIn;
 		const y = (numIn * oracleRateXY) / 1_000_000n;
-		expect(x > y).toBe(true);
+		expect(x < y).toBe(true); //Tracking R6: true => '<'| false => '>'
 		const trackingHeightOut = BigInt(mockChain.height);
 		const trackingHeightIn = BigInt(Number.MAX_SAFE_INTEGER);
 
@@ -147,7 +56,6 @@ describe('TrackingSpec', () => {
 		fundingParty.addBalance({
 			nanoergs: fakeNanoErgs
 		});
-
 		const oracleParty = mockChain.addParty(fakeScriptErgoTree, 'Oracle');
 		oracleParty.addBalance(
 			{
@@ -158,7 +66,6 @@ describe('TrackingSpec', () => {
 				R4: SLong(oracleRateXY).toHex()
 			}
 		);
-
 		const lpParty = mockChain.addParty(lpErgoTree, 'Lp');
 		lpParty.addBalance(
 			{
@@ -183,7 +90,7 @@ describe('TrackingSpec', () => {
 			{
 				R4: SInt(Number(numIn)).toHex(),
 				R5: SInt(Number(denomIn)).toHex(),
-				R6: SBool(false).toHex(),
+				R6: SBool(true).toHex(), //Tracking R6: true = less | false = more
 				R7: intMaxHex
 				//R7: SInt(Number(trackingHeightIn)).toHex()
 			}
@@ -191,6 +98,90 @@ describe('TrackingSpec', () => {
 
 		const trackingOut = new OutputBuilder(minStorageRent, trackingAddress)
 			.addTokens([{ tokenId: tracking98NFT, amount: 1n }])
+			.setAdditionalRegisters({
+				R4: SInt(Number(numIn)).toHex(),
+				R5: SInt(Number(denomIn)).toHex(),
+				R6: SBool(true).toHex(),
+				R7: SInt(Number(trackingHeightOut)).toHex()
+			});
+
+		const mainInputs = [...trackingParty.utxos, ...fundingParty.utxos];
+		const dataInputs = [...oracleParty.utxos, ...lpParty.utxos];
+
+		const tx = new TransactionBuilder(mockChain.height)
+			.from(mainInputs)
+			.withDataFrom(dataInputs)
+			.to(trackingOut)
+			.payFee(fee)
+			.sendChangeTo(changeAddress)
+			.build();
+
+		const executed = mockChain.execute(tx, { throw: false });
+		expect(executed).toBe(true);
+	});
+	it.only('Trigger 101% tracker should work', () => {
+		// Tracking101 => lpRate > 101 * Oracle
+		// Tracking101  R6: false
+		const lpInCirc = 10_000n;
+		const oracleRateXY = 9_088n * 1_000_000n;
+		const lpBalance = 10_000_000n;
+		const reservesX = 10_000_000_000n;
+		const reservesY = 1_000_000n;
+		const numIn = 101n;
+		const denomIn = 100n;
+		const lpRateXY = reservesX / reservesY;
+		const x = lpRateXY * denomIn;
+		const y = (numIn * oracleRateXY) / 1_000_000n;
+		expect(x > y).toBe(true); //Tracking R6: true => '<'| false => '>'
+		const trackingHeightOut = BigInt(mockChain.height);
+		const trackingHeightIn = BigInt(Number.MAX_SAFE_INTEGER);
+
+		const fundingParty = mockChain.addParty(fakeScriptErgoTree, 'Funding');
+		fundingParty.addBalance({
+			nanoergs: fakeNanoErgs
+		});
+		const oracleParty = mockChain.addParty(fakeScriptErgoTree, 'Oracle');
+		oracleParty.addBalance(
+			{
+				nanoergs: minStorageRent,
+				tokens: [{ tokenId: oraclePoolNFT, amount: 1n }]
+			},
+			{
+				R4: SLong(oracleRateXY).toHex()
+			}
+		);
+		const lpParty = mockChain.addParty(lpErgoTree, 'Lp');
+		lpParty.addBalance(
+			{
+				nanoergs: reservesX,
+				tokens: [
+					{ tokenId: lpNFT, amount: 1n },
+					{ tokenId: lpToken, amount: lpBalance },
+					{ tokenId: dexyUSD, amount: reservesY }
+				]
+			},
+			{
+				R4: SLong(lpInCirc).toHex()
+			}
+		);
+
+		const trackingParty = mockChain.addParty(trackingErgoTree, 'Tracking');
+		trackingParty.addBalance(
+			{
+				nanoergs: minStorageRent,
+				tokens: [{ tokenId: tracking101NFT, amount: 1n }]
+			},
+			{
+				R4: SInt(Number(numIn)).toHex(),
+				R5: SInt(Number(denomIn)).toHex(),
+				R6: SBool(false).toHex(), //true = less | false = more
+				R7: intMaxHex
+				//R7: SInt(Number(trackingHeightIn)).toHex()
+			}
+		);
+
+		const trackingOut = new OutputBuilder(minStorageRent, trackingAddress)
+			.addTokens([{ tokenId: tracking101NFT, amount: 1n }])
 			.setAdditionalRegisters({
 				R4: SInt(Number(numIn)).toHex(),
 				R5: SInt(Number(denomIn)).toHex(),
@@ -213,7 +204,7 @@ describe('TrackingSpec', () => {
 		expect(executed).toBe(true);
 	});
 
-	it.only('Trigger 98% tracker should fail if lp price is not below', () => {
+	it.skip('Trigger 98% tracker should fail if lp price is not below', () => {
 		const lpInCirc = 10_000n;
 		//10_205n
 		//const oracleRateXY = 10_205n;

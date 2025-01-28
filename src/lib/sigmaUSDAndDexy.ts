932836aed15e7e36a78c55776e6f640f0c932047
c8e4 <c8e4@proton.me>
2025-01-28 14:10:00 +0100

    add types

--- src/lib/sigmaUSDAndDexy.ts
+++ src/lib/sigmaUSDAndDexy.ts
@@ -1,23 +1,43 @@
-export const FEE_UI = 10n; //0.1%
+// Constants
+export const FEE_UI = 10n; // 0.1%
 export const FEE_UI_DENOM = 100_00n;
 
-// Fee
-export function applyFee(inputERG: bigint, feeMining: bigint) {
+// Types
+type FeeResult = {
+	uiSwapFee: bigint;
+	contractERG: bigint;
+};
+
+type ReverseFeeResult = {
+	inputERG: bigint;
+	uiSwapFee: bigint;
+};
+
+type ReverseFeeSellResult = {
+	userERG: bigint;
+	uiSwapFee: bigint;
+};
+
+// Functions
+export function applyFee(inputERG: bigint, feeMining: bigint): FeeResult {
 	const uiSwapFee = (inputERG * FEE_UI) / FEE_UI_DENOM;
 	const contractERG = inputERG - feeMining - uiSwapFee;
 	return { uiSwapFee, contractERG };
 }
-export function reverseFee(contractERG: bigint, feeMining: bigint) {
+
+export function reverseFee(contractERG: bigint, feeMining: bigint): ReverseFeeResult {
 	const uiSwapFee = (contractERG * FEE_UI) / (FEE_UI_DENOM - FEE_UI);
 	const inputERG = contractERG + feeMining + uiSwapFee;
 	return { inputERG, uiSwapFee };
 }
-export function reverseFeeSell(contractERG: bigint, feeMining: bigint) {
+
+export function reverseFeeSell(contractERG: bigint, feeMining: bigint): ReverseFeeSellResult {
 	const uiSwapFee = (contractERG * FEE_UI) / FEE_UI_DENOM;
 	const userERG = contractERG - feeMining - uiSwapFee;
 	return { userERG, uiSwapFee };
 }
-export function applyFeeSell(inputERG: bigint, feeMining: bigint) {
+
+export function applyFeeSell(inputERG: bigint, feeMining: bigint): FeeResult {
 	const uiSwapFee = (inputERG * FEE_UI) / (FEE_UI_DENOM - FEE_UI);
 	const contractERG = inputERG + feeMining + uiSwapFee;
 	return { uiSwapFee, contractERG };

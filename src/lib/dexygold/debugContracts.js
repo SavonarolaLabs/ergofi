5c0d4f9f9446f5aa84eda5cbc2f8138639eddf06
c8e4 <c8e4@proton.me>
2025-01-29 19:11:29 +0100

    debugRedeem

--- src/lib/dexygold/debugContracts.js
+++ src/lib/dexygold/debugContracts.js
@@ -4,14 +4,31 @@ import fs from 'fs';
  * @param {import("@fleet-sdk/common").EIP12UnsignedTransaction} tx
  */
 export function debugMint(tx){
+	saveTx('mint', tx);
 
-    const OUTPUT_FILE = 'debug/mintTx.js'
-    const fileContent = `export const mintTx = ${JSON.stringify(tx, null, 2)}`;
+}
 
-	try {
-		fs.writeFileSync(OUTPUT_FILE, fileContent, 'utf-8');
-		console.log(`Generated file: ${OUTPUT_FILE}`);
-	} catch (writeError) {
-		console.error(`Failed to write output file: ${OUTPUT_FILE}`, writeError);
-	}
+/**
+ * @param {import("@fleet-sdk/common").EIP12UnsignedTransaction} tx
+ */
+export function debugRedeem(tx){
+	saveTx('redeem', tx);
+}
+
+/**
+ * Saves a transaction object to a JavaScript file.
+ * 
+ * @param {string} name - The name of the transaction.
+ * @param {import("@fleet-sdk/common").EIP12UnsignedTransaction} tx - The unsigned transaction object.
+ */
+function saveTx(name, tx) {
+    const OUTPUT_FILE = `debug/${name}Tx.js`;
+    const fileContent = `export const ${name}Tx = ${JSON.stringify(tx, null, 2)}`;
+
+    try {
+        fs.writeFileSync(OUTPUT_FILE, fileContent, 'utf-8');
+        console.log(`Generated file: ${OUTPUT_FILE}`);
+    } catch (writeError) {
+        console.error(`Failed to write output file: ${OUTPUT_FILE}`, writeError);
+    }
 }

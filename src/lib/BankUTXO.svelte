40acdb2ec1bf9d17716de16fb9dfa6db09d3341e
c8e4 <c8e4@proton.me>
2025-02-07 19:30:06 +0100

    rework BankUTXO

--- src/lib/BankUTXO.svelte
+++ src/lib/BankUTXO.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Bank from './icons/Bank.svelte';
-	import Spinner from './Spinner.svelte';
 	import {
 		bank_price_rsv_buy,
 		bank_price_rsv_sell,
@@ -13,96 +11,65 @@
 		reserve_rate
 	} from './stores/bank';
 	import SubNumber from './SubNumber.svelte';
-	import { formatAmount, nanoErgToErg, oracleRateToUsd } from './utils';
+	import { formatAmount } from './utils';
 
 	export let confirmed = true;
-
-	const bank = {
-		type: 'UTxO',
-		price: 1.77,
-		reserveRatio: 551,
-		amount: 1230000,
-		ergAmount: 1647597
-	};
 </script>
 
-<div class="row flex flex-col gap-1 text-gray-500">
-	<div class="text-md flex items-end justify-between">
-		<div class="flex items-center gap-2 text-gray-500">
-			<Bank></Bank>
-			<div>{$reserve_rate}% Reserve</div>
-		</div>
-		<div class="flex items-center">
-			<div>
-				{nanoErgToErg($bank_box_nano_erg, 0)}
+<div class="row flex flex-col gap-1 text-gray-500" style="font-variant-numeric: tabular-nums;">
+	<a
+		href="https://sigmaspace.io/en/address/MUbV38YgqHy7XbsoXWF5z7EZm524Ybdwe5p9WDrbhruZRtehkRPT92imXer2eTkjwPDfboa1pR3zb3deVKVq3H7Xt98qcTqLuSBSbHb7izzo5jphEpcnqyKJ2xhmpNPVvmtbdJNdvdopPrHHDBbAGGeW7XYTQwEeoRfosXzcDtiGgw97b2aqjTsNFmZk7khBEQywjYfmoDc9nUCJMZ3vbSspnYo3LarLe55mh2Np8MNJqUN9APA6XkhZCrTTDRZb1B4krgFY1sVMswg2ceqguZRvC9pqt3tUUxmSnB24N6dowfVJKhLXwHPbrkHViBv1AKAJTmEaQW2DN1fRmD9ypXxZk8GXmYtxTtrj3BiunQ4qzUCu1eGzxSREjpkFSi2ATLSSDqUwxtRz639sHM6Lav4axoJNPCHbY8pvuBKUxgnGRex8LEGM8DeEJwaJCaoy8dBw9Lz49nq5mSsXLeoC4xpTUmp47Bh7GAZtwkaNreCu74m9rcZ8Di4w1cmdsiK1NWuDh9pJ2Bv7u3EfcurHFVqCkT3P86JUbKnXeNxCypfrWsFuYNKYqmjsix82g9vWcGMmAcu5nagxD4iET86iE2tMMfZZ5vqZNvntQswJyQqv2Wc6MTh4jQx1q2qJZCQe4QdEK63meTGbZNNKMctHQbp3gRkZYNrBtxQyVtNLR8xEY8zGp85GeQKbb37vqLXxRpGiigAdMe3XZA4hhYPmAAU5hpSMYaRAjtvvMT3bNiHRACGrfjvSsEG9G2zY5in2YWz5X9zXQLGTYRsQ4uNFkYoQRCBdjNxGv6R58Xq74zCgt19TxYZ87gPWxkXpWwTaHogG1eps8WXt8QzwJ9rVx6Vu9a5GjtcGsQxHovWmYixgBU8X9fPNJ9UQhYyAWbjtRSuVBtDAmoV1gCBEPwnYVP5GCGhCocbwoYhZkZjFZy6ws4uxVLid3FxuvhWvQrVEDYp7WRvGXbNdCbcSXnbeTrPMey1WPaXX"
+		target="_blank"
+	>
+		<div class="flex items-end justify-between">
+			<div class="items-left flex flex-col">
+				<div class="text-xs text-gray-600">Contract Address</div>
+				<div>MUb...aXX</div>
 			</div>
-			<div class="currency">ERG</div>
-		</div>
-	</div>
-
-	<div class="flex items-end justify-between text-lg">
-		<div class="flex items-center gap-1 text-lg uppercase">
-			<span class="price-left flex items-center gap-1">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 320 512"
-					width="0.6em"
-					fill="currentColor"
-					><path
-						d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"
-					/></svg
-				>
-				{$bank_price_usd_sell}</span
-			>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 320 512"
-				width="0.6em"
-				fill="currentColor"
-				><path
-					d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
-				/></svg
-			>
-			{$bank_price_usd_buy}
-		</div>
-		<div class="flex items-center font-normal">
-			<div>
-				{formatAmount($reserve_border_left_USD, false)}
+			<div class="items-left flex flex-col" style="margin-left:-20px">
+				<div class="text-xs text-gray-600">Reserve Rate</div>
+				<div>{$reserve_rate}%</div>
 			</div>
-			<div class="currency text-lg">SigUSD</div>
+			<div class="items-left flex flex-col">
+				<div class="text-xs text-gray-600">Bank Reserve</div>
+				<div class="items-left flex">
+					<div>
+						{formatAmount($bank_box_nano_erg / 10n ** 9n, false)}
+					</div>
+					<div class="currency">ERG</div>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="flex items-end justify-between text-lg">
-		<div class="flex items-center gap-1 uppercase">
-			<span class="price-left flex items-center gap-1">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 320 512"
-					width="0.6em"
-					fill="currentColor"
-					><path
-						d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"
-					/></svg
-				>
+	</a>
 
+	<div class="mt-2 flex items-end justify-between">
+		<div class="items-left flex flex-col">
+			<div class="text-xs text-gray-600">Buy Price</div>
+			<span class="price-left items-left flex gap-1"> {$bank_price_usd_sell}</span>
+			<span class="price-left items-left flex gap-1">
 				<SubNumber value={1 / $bank_price_rsv_buy}></SubNumber></span
 			>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 320 512"
-				width="0.6em"
-				fill="currentColor"
-				><path
-					d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
-				/></svg
-			>
+		</div>
+		<div class="items-left flex flex-col">
+			<div class="text-xs text-gray-600">Sell Price</div>
+			<span class="price-left items-left flex gap-1"> {$bank_price_usd_buy}</span>
 			<SubNumber value={1 / $bank_price_rsv_sell}></SubNumber>
 		</div>
-		<div class="flex items-center font-normal">
-			<div>
-				{formatAmount($reserve_border_right_RSV, false)}
+
+		<div class="items-left flex flex-col">
+			<div class="text-xs text-gray-600">Mintable Assets</div>
+			<div class="items-left flex">
+				<div>
+					{formatAmount($reserve_border_left_USD, false)}
+				</div>
+				<div class="currency">SigUSD</div>
 			</div>
-			<div class="currency text-lg">SigRSV</div>
+			<div class="items-left flex">
+				<div>
+					{formatAmount($reserve_border_right_RSV, false)}
+				</div>
+				<div class="currency">SigRSV</div>
+			</div>
 		</div>
 	</div>
 </div>

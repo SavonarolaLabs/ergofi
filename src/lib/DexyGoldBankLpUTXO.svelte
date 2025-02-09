9775d5a0a2919ee0e26a8303a790233fbc6591f2
Savonarola <x@x.com>
2025-02-09 15:48:21 +0400

    dexy widget Numbers divs

--- src/lib/DexyGoldBankLpUTXO.svelte
+++ src/lib/DexyGoldBankLpUTXO.svelte
@@ -12,7 +12,32 @@
 </script>
 
 <div class="text-sm">
-	{#if $dexygold_widget_numbers}{/if}
+	{#if $dexygold_widget_numbers}
+		<div>{$dexygold_widget_numbers.bankAvailableDexy} bankAvailableDexy</div>
+		<div>{$dexygold_widget_numbers.lpAvailabeDexy} lpAvailabeDexy</div>
+		<div>{$dexygold_widget_numbers.lpAvailabeErg} lpAvailabeErg</div>
+		<div>{$dexygold_widget_numbers.lpRate} lpRate</div>
+		<div class="mb-1"></div>
+		<div>{$dexygold_widget_numbers.bankFreeMintActivationRate} bankFreeMintActivationRate</div>
+		<div>{$dexygold_widget_numbers.oracleRate} oracleRate</div>
+		<div>{$dexygold_widget_numbers.bankArbMintActivationRate} bankArbMintActivationRate</div>
+		<div class="mb-1"></div>
+		<div>{$dexygold_widget_numbers.bankFreeMintExchangeRate} bankFreeMintExchangeRate</div>
+		<div>{$dexygold_widget_numbers.bankFreeMintResetHeight} bankFreeMintResetHeight</div>
+		<div>{$dexygold_widget_numbers.bankFreeMintAvailableDexy} bankFreeMintAvailableDexy</div>
+		<div>{$dexygold_widget_numbers.bankFreeMintResetDexy} bankFreeMintResetDexy</div>
+		<div>{$dexygold_widget_numbers.bankArbMintExchangeRate} bankArbMintExchangeRate</div>
+		<div>{$dexygold_widget_numbers.bankArbMintResetHeight} bankArbMintResetHeight</div>
+		<div>{$dexygold_widget_numbers.bankArbMintActivationHeight} bankArbMintActivationHeight</div>
+		<div>{$dexygold_widget_numbers.bankArbMintAvailableDexy} bankArbMintAvailableDexy</div>
+		<div>{$dexygold_widget_numbers.bankArbMintResetDexy} bankArbMintResetDexy</div>
+		<div>{$dexygold_widget_numbers.tracking101TriggerHeight} tracking101TriggerHeight</div>
+		<div>
+			{$dexygold_widget_numbers.isBankArbMintActivationHeightTriggered} isBankArbMintActivationHeightTriggered
+		</div>
+		<div>{$dexygold_widget_numbers.isBankArbMintActive} isBankArbMintActive</div>
+		<div>{$dexygold_widget_numbers.isBankFreeMintActive} isBankFreeMintActive</div>
+	{/if}
 </div>
 
 <style>

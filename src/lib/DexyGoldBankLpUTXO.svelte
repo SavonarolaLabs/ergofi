061c394000a43fb898967c5bd02b7122a1888386
Savonarola <x@x.com>
2025-02-09 15:34:56 +0400

    clean

--- src/lib/DexyGoldBankLpUTXO.svelte
+++ src/lib/DexyGoldBankLpUTXO.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import Bank from './icons/Bank.svelte';
-	import Tint from './icons/Tint.svelte';
-	import Spinner from './Spinner.svelte';
-	import {
-		bank_price_rsv_buy,
-		bank_price_rsv_sell,
-		bank_price_usd_buy,
-		bank_price_usd_sell,
-		bank_box_nano_erg,
-		oracle_price_sig_usd_cent,
-		reserve_border_left_USD,
-		reserve_border_right_RSV,
-		reserve_rate
-	} from './stores/bank';
-	import SubNumber from './SubNumber.svelte';
-	import { formatAmount, nanoErgToErg, oracleRateToUsd } from './utils';
 	import { calculateDexyGoldWidgetNumbers, dexygold_widget_numbers } from './stores/dexyGoldStore';
 	import { initJsonTestBoxes } from './stores/dexyGoldStoreJsonTestData';
-	import { entries } from 'lodash-es';
 
 	export let confirmed = true;
 

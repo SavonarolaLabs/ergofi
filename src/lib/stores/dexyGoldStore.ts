6e3ce9644b2005d7dc3c00b09e39bf541cb2fdda
Savonarola <x@x.com>
2025-02-09 14:11:13 +0400

    DexyGoldWidgetNumbers

--- src/lib/stores/dexyGoldStore.ts
+++ src/lib/stores/dexyGoldStore.ts
@@ -16,3 +16,28 @@ export const dexygold_lp_mint_box = writable<any>();
 export const dexygold_lp_redeem_box = writable<any>();
 export const dexygold_lp_extract_box = writable<any>();
 export const dexygold_lp_swap_box = writable<any>();
+
+export const dexygold_widget_numbers = writable<DexyGoldWidgetNumbers>();
+
+type DexyGoldWidgetNumbers = {
+	bankAvailableDexy: bigint;
+	lpAvailabeDexy: bigint;
+	lpAvailabeErg: bigint;
+	lpRate: number;
+	oracleRate: number;
+
+	bankFreeMintExchangeRate: number;
+	bankFreeMintActivationRate: number;
+	bankFreeMintResetHeight: number;
+	bankFreeMintAvailableDexy: bigint;
+	bankFreeMintResetDexy: bigint;
+
+	bankArbMintExchangeRate: number;
+	bankArbMintActivationRate: number;
+	bankArbMintResetHeight: number;
+	bankArbMintActivationHeight: number; //Tracking 101 height + T_arb
+	bankArbMintAvailableDexy: bigint;
+	bankArbMintResetDexy: bigint;
+
+	tracking101TriggerHeight: number;
+};

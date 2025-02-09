b00b368b31e4ec7f8f4a9c4d9f9af7745060ab0d
Savonarola <x@x.com>
2025-02-09 15:02:05 +0400

    wip widget

--- src/lib/stores/dexyGoldStore.ts
+++ src/lib/stores/dexyGoldStore.ts
@@ -1,4 +1,13 @@
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
+import {
+	parseBankArbitrageMintBox,
+	parseBankBox,
+	parseBankFreeMintBox,
+	parseDexyGoldOracleBox,
+	parseLpBox,
+	parseTrackingBox
+} from './dexyGoldParser';
+import { DEXY_GOLD } from '$lib/dexygold/dexyConstants';
 
 export const dexygold_lp_box = writable<any>();
 export const oracle_erg_xau_box = writable<any>();
@@ -23,21 +32,91 @@ type DexyGoldWidgetNumbers = {
 	bankAvailableDexy: bigint;
 	lpAvailabeDexy: bigint;
 	lpAvailabeErg: bigint;
-	lpRate: number;
-	oracleRate: number;
-
+	lpRate: bigint;
+	oracleRate: bigint;
 	bankFreeMintExchangeRate: number;
-	bankFreeMintActivationRate: number;
-	bankFreeMintResetHeight: number;
+	bankFreeMintActivationRate: bigint;
+	bankFreeMintResetHeight: bigint;
 	bankFreeMintAvailableDexy: bigint;
 	bankFreeMintResetDexy: bigint;
-
 	bankArbMintExchangeRate: number;
-	bankArbMintActivationRate: number;
-	bankArbMintResetHeight: number;
-	bankArbMintActivationHeight: number; //Tracking 101 height + T_arb
+	bankArbMintActivationRate: bigint;
+	bankArbMintResetHeight: bigint;
+	bankArbMintActivationHeight: bigint; //Tracking 101 height + T_arb
 	bankArbMintAvailableDexy: bigint;
 	bankArbMintResetDexy: bigint;
-
-	tracking101TriggerHeight: number;
+	tracking101TriggerHeight: bigint;
+	isBankArbMintActive: boolean;
+	isBankFreeMintActive: boolean;
 };
+
+function calculateDexyGoldWidgetNumbers() {
+	const arbMintIn = get(dexygold_bank_arbitrage_mint_box);
+	const { R4ResetHeight: bankArbMintResetHeight, R5AvailableAmount: bankArbMintAvailableDexy } =
+		parseBankArbitrageMintBox(arbMintIn);
+
+	const freeMintIn = get(dexygold_bank_free_mint_box);
+	const { R4ResetHeight: bankFreeMintResetHeight, R5AvailableAmount: bankFreeMintAvailableDexy } =
+		parseBankFreeMintBox(freeMintIn);
+
+	const bankIn = get(dexygold_bank_box);
+	const { dexyAmount: bankAvailableDexy } = parseBankBox(bankIn);
+
+	const lpIn = get(dexygold_lp_box);
+	const { dexyAmount: lpYIn, value: lpXIn, lpTokenAmount } = parseLpBox(lpIn);
+
+	const goldOracle = get(oracle_erg_xau_box);
+	const { R4Rate: oracleRate } = parseDexyGoldOracleBox(goldOracle);
+
+	const tracking101 = get(dexygold_tracking101_box);
+	const { R7TriggeredHeight: tracking101TriggerHeight } = parseTrackingBox(tracking101);
+
+	const bankFreeMintResetDexy = lpYIn / 100n; //free
+
+	const oracleRateXy = oracleRate / 1_000_000n; //<== 1_000_000
+	const bankRate =
+		(oracleRateXy * (DEXY_GOLD.bankFeeNum + DEXY_GOLD.feeDenom)) / DEXY_GOLD.feeDenom;
+	const buybackRate = (oracleRateXy * DEXY_GOLD.bankFeeNum) / DEXY_GOLD.feeDenom;
+	const oracleRateWithFee = bankRate + buybackRate;
+
+	const bankArbMintResetDexy = (lpXIn - oracleRateWithFee * lpYIn) / oracleRateWithFee; //arb
+
+	const bankArbMintActivationRate = (101n * oracleRateWithFee) / 100n;
+	const bankFreeMintActivationRate = (oracleRate * 98n) / 100n;
+
+	const lpRateXy = lpXIn / lpYIn;
+	const isBankArbMintActivationRateTriggered = lpRateXy * 100n > 101n * oracleRateWithFee;
+	const isBankFreeMintActivationRateTriggered = lpRateXy * 100n > oracleRate * 98n;
+
+	const bankArbMintActivationHeight = tracking101TriggerHeight + DEXY_GOLD.T_arb;
+
+	const isBankArbMintActive = isBankArbMintActivationRateTriggered && bankArbMintActivationHeight;
+	const isBankFreeMintActive = isBankFreeMintActivationRateTriggered;
+
+	const bankFreeMintExchangeRate = -666;
+	const bankArbMintExchangeRate = -666;
+
+	// oracleRate <=
+
+	dexygold_widget_numbers.set({
+		bankAvailableDexy,
+		lpAvailabeDexy: lpYIn,
+		lpAvailabeErg: lpXIn,
+		lpRate: lpRateXy,
+		oracleRate: oracleRateXy,
+		bankFreeMintExchangeRate,
+		bankFreeMintActivationRate,
+		bankFreeMintResetHeight,
+		bankFreeMintAvailableDexy,
+		bankFreeMintResetDexy,
+		bankArbMintExchangeRate,
+		bankArbMintActivationRate,
+		bankArbMintResetHeight,
+		bankArbMintActivationHeight,
+		bankArbMintAvailableDexy,
+		bankArbMintResetDexy,
+		tracking101TriggerHeight,
+		isBankArbMintActive,
+		isBankFreeMintActive
+	});
+}

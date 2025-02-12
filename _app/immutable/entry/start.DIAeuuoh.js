41083965aac481a14fb5782c48fa87f8af3b55d3
c8e4 <c8e4@proton.me>
2025-02-12 10:15:02 +0100

    Updates

---
+++ _app/immutable/entry/start.DIAeuuoh.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/qGCUU99I.js";export{t as start};
---
+++ sw.js
@@ -0,0 +1,6 @@
+// static/sw.js
+self.addEventListener('install', (event) => {});
+
+self.addEventListener('activate', (event) => {});
+
+self.addEventListener('fetch', (event) => {});

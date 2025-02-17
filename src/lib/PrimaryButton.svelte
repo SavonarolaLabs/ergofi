b7f5fc5fc6ac4e9c560ab0bff812ea6a4f24d45a
c8e4 <c8e4@proton.me>
2025-02-17 09:46:42 +0100

    css

--- src/lib/PrimaryButton.svelte
+++ src/lib/PrimaryButton.svelte
@@ -8,7 +8,7 @@
 <button class="btn" on:click={onClick}>
 	<span class="btn__content" style="background-color: {bgColor};">{text}</span>
 	<span class="btn__glitch" style="background-color: {bgColor};"></span>
-	<span class="btn__label" style="background-color: {bgColor}; color:black;">{subtext}</span>
+	<span class="btn__label" style="background-color: {bgColor};">{subtext}</span>
 </button>
 
 <style>
